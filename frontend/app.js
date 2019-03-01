9c80329be4989d61ce61478aa92db8b3218acc12
Agus Ekanurdi <aguseka@gmail.com>
2019-03-01 16:22:03 +0800

    Add AWS Cognito

--- frontend/app.js
+++ frontend/app.js
@@ -1,3 +1,42 @@
+const IDTokenSessionKey = 'AA_BillyWebIDToken';
+const AWSCognitoUserPool = {
+	UserPoolId : 'us-east-1_cXIjmWeI4',
+	ClientId : '5siaa3ns06grv3jhfnnncq4t7j'
+};
+const AWSCognitoProvider = 'cognito-idp.us-east-1.amazonaws.com/'.concat(AWSCognitoUserPool.UserPoolId);
+const AWSCognitoIdentityPoolID = 'us-east-1:1491e474-f9b3-4d19-821b-b6d93466a085';
+const AWSCognitoLoginURL = 'https://embryo-billy.auth.us-east-1.amazoncognito.com/login?response_type=token&client_id='+AWSCognitoUserPool.ClientId+'&redirect_uri=https://dnx78czn5g853.cloudfront.net';
+
+AWS.config.region = 'us-east-1';
+
+$(document).ready(function () {
+	const url = window.location.href;
+	if (url.indexOf('#id_token=') < 0 && !sessionStorage.getItem(IDTokenSessionKey))
+		window.location.replace(AWSCognitoLoginURL);
+
+	if (!sessionStorage.getItem(IDTokenSessionKey)) {
+		const startIdx = url.indexOf('#id_token=') + '#id_token='.length;
+		const tokenInfo = url.substring(startIdx, url.length).split('&');
+		sessionStorage.setItem(IDTokenSessionKey, tokenInfo[0]);
+	}
+
+	let provider = AWSCognitoProvider;
+	let login = {};
+	login[provider] = sessionStorage.getItem(IDTokenSessionKey);
+
+	let credentials = new AWS.CognitoIdentityCredentials({
+		IdentityPoolId: AWSCognitoIdentityPoolID,
+		Logins: login
+	});
+
+	credentials.get((error) => {
+		if (error) {
+			sessionStorage.removeItem(IDTokenSessionKey);
+			window.location.replace(AWSCognitoLoginURL);
+		}
+	});
+});
+
 const backendURLPath = 'http://18.204.42.219:8080';
 const resizeWidth = 640;
 const resizeHeight = 480;
